797c3707e6556a782ce4d1a3c5dff073371f66c7
jch9537 <jch9537@gmail.com>
2020-03-13 21:31:35 +0900

    Updates

---
+++ precache-manifest.dca856db6622497f5e4049eb40277ac9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c05fbd4ccd0e6a3690b47e926357ea",
+    "url": "/test1/index.html"
+  },
+  {
+    "revision": "631c33fee79c7cd265f4",
+    "url": "/test1/static/js/2.1a4a4042.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/test1/static/js/2.1a4a4042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b105ecf3a6dc79fdcb6e",
+    "url": "/test1/static/js/main.e9f91af7.chunk.js"
+  },
+  {
+    "revision": "ecf46d6cce7e97cbff81",
+    "url": "/test1/static/js/runtime-main.c81bdfb4.js"
+  }
+]);
